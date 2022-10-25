cce7eb93c0132975af9eb8be39660bc48cc37a61
Chintiamor <moreira.cinthia.9347@gmail.com>
2022-10-25 19:48:27 -0400

    Ultima version de interfazz agregar cliente

--- application/views/cliente/cliente_agregar.php
+++ application/views/cliente/cliente_agregar.php
@@ -1,5 +1,5 @@
 <div class="content-wrapper">
-  <div class="container">
+  <div class="container-fluid">
     <div class="row justify-content-md-center">
       <div class="col col-lg-6">
 
@@ -13,25 +13,41 @@
             <form method="post" id="add_create" name="add_create" action="<?= site_url('cliente/crearCliente') ?>">
 
               <div class="form-group">
-                <label for="nombre" class="form-label">Nombre o Razon Social *</label>
-                <input id="nombre" type="text" name="nombre_razonSocial" class="form-control" placeholder="Escriba el Nombre" required>
+                <label for="nombre" class="form-label">Nombre/s o Razon Social *</label>
+                <input id="nombre" type="text" name="nombres" class="form-control" placeholder="Escriba el Nombre" required>
               </div>              
 
               <div class="form-group">
-                <label>NIT o Carnet de Identidad *</label>
-                <input type="text" name="nit_carnet" class="form-control" placeholder="Escriba su numero de carnet de identidad" minlength="6"  required>
+                <div class="row">
+                  <div class="col-md-6">
+                    <label for="nombre" class="form-label">Primer Apellido</label>
+                    <input id="primerAp" type="text" name="primerApellido" class="form-control" placeholder="Escriba el Primer Apellido">
+                  </div>
+                  <div class="col-md-6">
+                    <label for="nombre" class="form-label">Segundo Apellido</label>
+                    <input id="segundoAp" type="text" name="segundoApellido" class="form-control" placeholder="Escriba el segundo Apellido">
+                  </div>
+                </div>
+              </div> 
+
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-md-6">
+                    <label>NIT o Carnet de Identidad *</label>
+                    <input type="text" name="nit_carnet" class="form-control" placeholder="Escriba su numero de carnet de identidad" minlength="6"  required>
+                  </div>
+                  <div class="col-md-6">
+                    <label>Telefono</label>
+                    <input type="text" name="telefono" class="form-control" placeholder="Escriba el numero de telefono">
+                  </div>
+                </div>
               </div>
 
               <div class="form-group">
                 <label>Direccion</label>
                 <input type="text" name="direccion" class="form-control" placeholder="Escriba la dirección">
               </div>
-
-              <div class="form-group">
-                <label>Telefono</label>
-                <input type="text" name="telefono" class="form-control" placeholder="Escriba el numero de telefono">
-              </div>
-           
+              <p>(*) Campos obligatorios</p>           
               <button type="submit" class="btn btn-primary">Crear Cliente</button>
             </form>
           </div>
