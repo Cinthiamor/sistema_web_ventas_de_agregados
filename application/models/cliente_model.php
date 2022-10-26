901b9a1615217ec96b50ce5b52adc9048430b17a
Chintiamor <moreira.cinthia.9347@gmail.com>
2022-10-26 05:25:00 -0400

    modificacion de la ultima version de cliente_model.php en el modelo

--- application/models/cliente_model.php
+++ application/models/cliente_model.php
@@ -4,20 +4,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Cliente_model extends CI_Model {
 
     public $estado;
-    public $fechaCreacion;
+    public $fechaRegistro;
     public $fechaActualizacion;
 
     public function __construct(){
         $this->estado = true;
-        $this->fechaCreacion = date("Y-m-d H:i:s");
+        $this->fechaRegistro = date("Y-m-d H:i:s");
         $this->fechaActualizacion = date("Y-m-d H:i:s");
     }
 
+
+
+
+
     public function lista()
 	{
-		$this->db->select('*');
-        $this->db->from('cliente'); 
-        $this->db->where('estado', 1);       
+		$this->db->select('idCliente, CONCAT(c.nombres," ",c.primerApellido," ",IFNULL(c.segundoApellido,"")) as nombre_razonSocial, nit_carnet,telefono, direccion');
+        $this->db->from('cliente c');
+        $this->db->where('c.estado', 1);
+        $this->db->where('c.habilitado', 1);
         return $this->db->get();
 	}
 
@@ -28,8 +33,21 @@ class Cliente_model extends CI_Model {
         return $this->db->get();
 	}
 
+     public function recuperarClienteRecibo($idCliente){
+        $this->db->select('*');
+        return $this->db->get_where('cliente',array('idCliente'=>$idCliente))->row_array();
+    }
+
+    public function recuperarClienteVenta($nit_carnet){
+        return $this->db->get_where('cliente',array('nit_carnet'=>$nit_carnet))->row_array();
+    }
+
+    public function recuperarClientePorID($idCliente){
+        return $this->db->get_where('cliente',array('idCliente'=>$idCliente))->row_array();
+    }
+
     public function crearCliente($data) {
-        $data['fechaCreacion'] =  $this->fechaCreacion;
+        $data['fechaRegistro'] =  $this->fechaRegistro;
         $data['estado'] = $this->estado;
         $this->db->insert('cliente', $data); 
     }
@@ -40,9 +58,9 @@ class Cliente_model extends CI_Model {
         $this->db->update('cliente', $data); 
     }
 
-    public function eliminarCliente($idCliente)
+    public function eliminarCliente($idCliente, $estado)
     {
-        $data['estado'] = !$this->estado;
+        $data['estado'] = $estado;
         $data['fechaActualizacion'] = $this->fechaActualizacion;
         $this->db->where('idCliente', $idCliente);
         $this->db->update('cliente', $data);
@@ -50,10 +68,27 @@ class Cliente_model extends CI_Model {
 
     public function listadeshabilitados()
 	{
-		$this->db->select('*');
-        $this->db->from('cliente'); 
-        $this->db->where('estado', 0);
+		$this->db->select('idCliente, CONCAT(c.nombres," ",c.primerApellido," ",c.segundoApellido) as nombre_razonSocial, nit_carnet,telefono, direccion');
+        $this->db->from('cliente c');
+        $this->db->where('estado', 1);
+        $this->db->where('habilitado', 0);
        
         return $this->db->get();
 	}
+    public function getClientes(){
+		$this->db->select("c.*");
+		$this->db->from("cliente c");
+		$this->db->where("c.estado","1");
+		$resultados = $this->db->get();
+		return $resultados->result();
+	}
+
+    function listarTodosClientes()
+    {
+        $this->db->where('estado', 1);
+        $this->db->where('habilitado', 1);
+        $this->db->order_by('idCliente', 'desc');
+        return $this->db->get('cliente'); //->result_array();
+    }
+
 }
